d89039177bcd668dfc8146f362c300b3c00e89a9
Divaldo <antifraudeuda@gmail.com>
2021-08-11 21:52:01 -0600

    Hola mundo

---
+++ apps/webrestaurante/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WebrestauranteConfig(AppConfig):
+    name = 'webrestaurante'
---
+++ apps/webrestaurante/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import init
+
+
+urlpatterns = {
+    path('', init, name='init'),
+}
