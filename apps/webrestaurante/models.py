9e2ed59a095b96050d0163a23af00d67ce923815
Divaldo <antifraudeuda@gmail.com>
2021-08-25 16:10:32 -0600

    CREACION MODELS

--- apps/webrestaurante/models.py
+++ apps/webrestaurante/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class Comida(models.Model):
     pk_comida = models.AutoField(primary_key=True, null=False, blank=False)
     nombre = models.CharField(max_length=50, null=False, blank=False)
@@ -23,6 +24,6 @@ class Postres(models.Model):
 class Menu(models.Model):
     pk_menu = models.AutoField(primary_key=True, null=False, blank=False)
     tiempo_comida = models.CharField(max_length=20, null=False, blank=False)
-    fk_Comida = models.ManyToManyField(Comida, null=False, blank=False, on_delete=models.CASCADE)
-    fk_Bebidas= models.ManyToManyField(Bebidas, null=False, blank=False, on_delete=models.CASCADE)
-    fk_Postres= models.ManyToManyField(Postres, null=False, blank=False, on_delete=models.CASCADE)
+    fk_Comida = models.ForeignKey(Comida, null=False, blank=False, on_delete=models.CASCADE)
+    fk_Bebidas = models.ForeignKey(Bebidas, null=False, blank=False, on_delete=models.CASCADE)
+    fk_Postres = models.ForeignKey(Postres, null=False, blank=False, on_delete=models.CASCADE)
