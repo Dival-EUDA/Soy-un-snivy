a72314fb1cf0707d464cd837c76cf010d789e79b
Divaldo <antifraudeuda@gmail.com>
2021-09-16 08:42:03 -0600

    heroku34

--- DevRestaurante/settings.py
+++ DevRestaurante/settings.py
@@ -76,8 +76,7 @@ WSGI_APPLICATION = 'DevRestaurante.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-
- """
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
